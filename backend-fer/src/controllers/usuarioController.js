576957699c7742f3e72c80a57f835c275309a6e7
raidentek <33987212+raidentek@users.noreply.github.com>
2020-09-02 22:29:27 -0500

    update

--- backend-fer/src/controllers/usuarioController.js
+++ backend-fer/src/controllers/usuarioController.js
@@ -1,16 +1,35 @@
 const UsuarioModel = require("../models/usuarioModel");
-const _ = require('underscore');
+const _ = require("underscore");
 
+//obtiene all usuarios
 exports.getUsuarios = async (req, res, next) => {
+
+  let limite = parseInt(req.query.limite) || 5; // convierte a un string a un numero primera forma
+
+  let desde = req.query.desde || 0;
+  desde = Number(desde); // convierte a un string a un numero segunda forma
+
   try {
-    const usuario = await UsuarioModel.find({}, { password: 0 });
-    res.json(usuario);
+    const usuario = await UsuarioModel.find({estado: true}, { password: 0 })
+      .skip(desde) //salta desde que numeros de datos quieres ver los datos
+      .limit(limite); // limita los datos que puedes obtener
+
+      // mostrando la cantidad total de datos con conteo  y estado solo lo que estan en true
+    await UsuarioModel.count({estado: true}, (err, conteo) => {
+      res.json({
+        ok: true,
+        usuario,
+        cuantos: conteo
+      })
+    })
+    
   } catch (err) {
     res.send(err);
     next();
   }
 };
 
+// create usuarios
 exports.postUsuario = async (req, res, next) => {
   try {
     const usuario = new UsuarioModel(req.body);
@@ -28,16 +47,41 @@ exports.postUsuario = async (req, res, next) => {
 
 //runValidators es para actualizar los valores deacuerdo al esquema de la db
 exports.updateUsuario = async (req, res, next) => {
+  //usando el underscore
+  // el pick te retorno los valores que tu quieres actualizar
+  let body = _.pick(req.body, ["nombre", "email", "img", "role", "estado"]);
+
   try {
     const usuario = await UsuarioModel.findByIdAndUpdate(
       { _id: req.params.IdUsuario },
-      req.body,
+      body,
       { new: true, runValidators: true }
     );
-    res.json(usuario)
+    res.json(usuario);
     // res.json({message: 'usuario update'})
   } catch (err) {
     res.send(err);
     next();
   }
 };
+
+//Elimina un usuario en forma de estado : false o true
+exports.deleteUsuario = async (req, res, next) => {
+
+  let cambiaEstado = {
+    estado: false
+  }
+
+  try {
+    const usuario = await UsuarioModel.findByIdAndUpdate({_id: req.params.IdUsuario}, cambiaEstado, {new: true} )
+ 
+        res.json({
+          ok: true,
+          message: `usuario ${usuario.nombre} desactive successfully`
+        }) 
+    
+  } catch (err) {
+    res.send(err)
+    next()
+  }
+}
